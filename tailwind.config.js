de0db654d0dd3d589e855db2457ce389024054af
Vilen Bejenaru <70964449+vilenbejenaru@users.noreply.github.com>
2023-02-28 20:26:46 +0300

    init project, install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    content: [
+        "app/**/*.php",
+        "./resources/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.cyan,
+                success: colors.emerald,
+                warning: colors.amber,
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
+};
