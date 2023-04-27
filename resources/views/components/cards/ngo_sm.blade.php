3dedbe3471f4892d8cf0d38b151480abd48298af
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-04-27 04:45:06 +0300

    small fix

--- resources/views/components/cards/ngo_sm.blade.php
+++ resources/views/components/cards/ngo_sm.blade.php
@@ -1,5 +1,5 @@
 <div class="card my-2 p-5 sm:p-10 md:w-[48%] lg:w-[32%]">
-    <img class="w-24" src="{{$ngo->getFirstMediaUrl()?: Vite::asset('resources/images/design/help.png') }}" alt="{{$ngo->name}}">
+    <img class="w-24" src="{{$ngo->getFirstMediaUrl()?: Vite::asset('resources/images/design/placeholder.png') }}" alt="{{$ngo->name}}">
 
     <h4 class="my-2 text-xl font-bold">{{$ngo->name}}</h4>
     <p>{{Str::limit($ngo->description,520,'...')}}</p>
