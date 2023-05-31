2c637722aee749817a881014372382acfe038ac3
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-05-31 16:19:18 +0300

    add gtag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @if(App::environment('production'))
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R0RSRB4PSE"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R0RSRB4PSE');
+        </script>
+    @endif
     <title>{{ $title ?? 'Help2Help' }}</title>
     <!-- Styles -->
     @vite(['resources/scss/app.scss'])
