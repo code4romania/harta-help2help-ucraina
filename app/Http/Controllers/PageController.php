dc4cd39f162da6703728fac44f80e72c65882a31
Andrei Ioniță <hi@andrei.io>
2023-08-23 16:43:51 +0100

    refactor pages

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,80 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\Ngo;
-use App\Models\Service;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class PageController extends Controller
 {
-    public function ngosPage(Request $request)
+    public function __invoke(string $locale, string $page, Request $request): View
     {
-        $attributes = $request->validate([
-            'search' => ['nullable', 'string'],
-            'filter.county' => ['nullable', 'exists:counties,id'],
-            'filter.intervention_domain' => ['nullable', 'exists:intervention_domains,id'],
-            'filter.beneficiary' => ['nullable', 'exists:beneficiary_groups,id'],
-        ]);
+        $view = "pages.$page";
 
-        return view('ngos', [
-            'ngos' => Ngo::searchAndFilter(
-                data_get($attributes, 'search'),
-                data_get($attributes, 'filter'),
-            )->paginate(6),
-        ]);
-    }
+        abort_unless(view()->exists($view), 404);
 
-    public function ngoPage(string $locale, Ngo $ngo)
-    {
-        return view('ngos_index', [
-            'ngo' => $ngo,
-            'breadcrumbs' => [
-                [
-                    'name' => __('txt.header.ngos'),
-                    'url' => localized_route('ngos'),
-                ],
-                [
-                    'name' => $ngo->name,
-                ],
-            ],
-        ]);
-    }
-
-    public function services(Request $request)
-    {
-        $attributes = $request->validate([
-            'search' => ['nullable', 'string'],
-            'filter.county' => ['nullable', 'exists:counties,id'],
-            'filter.intervention_domain' => ['nullable', 'exists:intervention_domains,id'],
-            'filter.beneficiary' => ['nullable', 'exists:beneficiary_groups,id'],
-            'filter.status' => ['nullable', 'string', 'in:active,finished'],
-        ]);
-
-        return view('services', [
-            'view' => 'map',
-            'services' => Service::searchAndFilter(
-                data_get($attributes, 'search'),
-                data_get($attributes, 'filter'),
-            )->get(),
-        ]);
-    }
-
-    public function servicesList(Request $request)
-    {
-        $attributes = $request->validate([
-            'search' => ['nullable', 'string'],
-            'filter.county' => ['nullable', 'exists:counties,id'],
-            'filter.intervention_domain' => ['nullable', 'exists:intervention_domains,id'],
-            'filter.beneficiary' => ['nullable', 'exists:beneficiary_groups,id'],
-            'filter.status' => ['nullable', 'string', 'in:active,finished'],
-        ]);
-
-        return view('services', [
-            'view' => 'list',
-            'services' => Service::searchAndFilter(
-                data_get($attributes, 'search'),
-                data_get($attributes, 'filter'),
-            )->paginate(),
-        ]);
+        return view($view);
     }
 }
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,42 +5,42 @@
         Help2Help About Page
         </x-slot>
         <div class="about-page ">
-            <section class=" container flex flex-col">
+            <section class="container flex flex-col ">
 
                 <div class="flex flex-col items-center justify-center md:flex-row">
 
                     <div class="w-2/5">
                         <img class="w-full p-3" src="{{ Vite::asset('resources/images/design/help-big.png') }}"
-                             alt="Logo">
+                            alt="Logo">
                     </div>
-                    <div class="container mx-auto flex w-full flex-col sm:p-5">
-                        <h4 class="mb-0 mt-5 self-center text-2xl"> {{ __('txt.footer.partners') }}</h4>
-                        <div class="my-6 flex items-center lg:w-11/12 justify-evenly ">
+                    <div class="container flex flex-col w-full mx-auto sm:p-5">
+                        <h4 class="self-center mt-5 mb-0 text-2xl"> {{ __('txt.footer.partners') }}</h4>
+                        <div class="flex items-center my-6 lg:w-11/12 justify-evenly ">
                             <a class="mx-2 md:mx-4" href="https://fonpc.ro/">
                                 <img class="h-8 md:h-10 lg:h-16"
-                                     src="{{ Vite::asset('resources/images/design/fonpc.png') }}" alt="fonpc"
-                                     target="_blank">
+                                    src="{{ Vite::asset('resources/images/design/fonpc.png') }}" alt="fonpc"
+                                    target="_blank">
                             </a>
                             <a class="mx-2 md:mx-4"
-                               href="https://www.care-international.org/our-work/where-we-work/romania">
+                                href="https://www.care-international.org/our-work/where-we-work/romania">
                                 <img class="h-12 md:h-16 lg:h-24 "
-                                     src="{{ Vite::asset('resources/images/design/care.png') }}" alt="care"
-                                     target="_blank">
+                                    src="{{ Vite::asset('resources/images/design/care.png') }}" alt="care"
+                                    target="_blank">
                             </a>
                             <a class="mx-2 md:mx-4" href="https://www.sera.ro/seraromania/en/">
                                 <img class="h-12 md:h-16 lg:h-24 "
-                                     src="{{ Vite::asset('resources/images/design/sera.png') }}" alt="sera"
-                                     target="_blank">
+                                    src="{{ Vite::asset('resources/images/design/sera.png') }}" alt="sera"
+                                    target="_blank">
                             </a>
                             <a class="mx-2 md:mx-4" href="https://code4.ro/en/donate/">
                                 <img class="h-8 md:h-10 lg:h-16"
-                                     src="{{ Vite::asset('resources/images/design/code4ro.png') }}" alt="code4ro"
-                                     target="_blank">
+                                    src="{{ Vite::asset('resources/images/design/code4ro.png') }}" alt="code4ro"
+                                    target="_blank">
                             </a>
                         </div>
                     </div>
                 </div>
-                <h2 class="text-xl md:text-2xl lg:text-3xl 2xl:text-4xl mt-5 "> {{ __('txt.about.about_project') }}</h2>
+                <h2 class="mt-5 text-xl md:text-2xl lg:text-3xl 2xl:text-4xl "> {{ __('txt.about.about_project') }}</h2>
                 <p>{{ __('txt.about.text') }}</p>
                 <p>{{ __('txt.about.text1') }}</p>
                 <p>{{ __('txt.about.text2') }}</p>
