cfee2aa2b92b4baf818c828dcd06f0155af8afd4
Andrei Ioniță <hi@andrei.io>
2023-08-23 16:48:37 +0100

    refactor header

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -13,12 +13,37 @@ class Header extends Component
 {
     public Collection $languages;
 
+    public array $menu;
+
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
         $this->languages = app('languages');
+
+        $this->menu = [
+            [
+                'name' => __('txt.header.home'),
+                'url' => localized_route('home'),
+            ],
+            [
+                'name' => __('txt.header.about_project'),
+                'url' => localized_route('page', ['page' => 'about']),
+            ],
+            [
+                'name' => __('txt.header.services_map'),
+                'url' => localized_route('services'),
+            ],
+            [
+                'name' => __('txt.header.ngos'),
+                'url' => localized_route('ngos.index'),
+            ],
+            [
+                'name' => __('txt.header.contact'),
+                'url' => localized_route('page', ['page' => 'contact']),
+            ],
+        ];
     }
 
     /**
